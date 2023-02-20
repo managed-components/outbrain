0d6d13684efcb041498fef45262821483c4f96a8
Omar Mohammad <omarmo@cloudflare.com>
2023-02-20 18:10:14 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
+
+interface OutbrainParams {
+  apiVersion: string,
+  obtpVersion: string,
+  optOut: boolean,
+  bust: string,
+  dl: string,
+  ob_click_id?: string | null,
+}
+
+export const eventHandler = async (eventType: string, event: MCEvent, settings: ComponentSettings) => {
+  const { client, payload } = event
+
+  const baseUrl = 'https://tr.outbrain.com/'
+  const unifiedPixel = 'unifiedPixel'
+
+  let params: OutbrainParams = {
+    apiVersion: '1.1',
+    obtpVersion: '1.4.1', // what is this?
+    optOut: false,
+    bust: Math.random().toString().replace('.', ''),
+    dl: encodeURIComponent(client.url.href),
+  }
+
+  // deal with dicbo param
+  const dlParams = new URLSearchParams(new URL(client.url.href).search)
+  if (dlParams.has('dicbo')) {
+    params['ob_click_id'] = dlParams.get('dicbo')
+  }
+
+  payload.name = payload.name.replace(/ /g, '-')
+  const stringifiedParams = new URLSearchParams({ ...payload, ...params }).toString()
+
+  fetch(`${baseUrl}${unifiedPixel}?${stringifiedParams}`)
+}
+
+export default async function (manager: Manager, settings: ComponentSettings) {
+  manager.addEventListener('pageview', event => {
+    eventHandler('pageview', event, settings)
+  })
+  manager.addEventListener('event', event => {
+    eventHandler('event', event, settings)
+  })
+}
