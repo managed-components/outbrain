3db76f979cc56403c0c6382617c2f804737e4125
Omar Mohammad <omarmo@cloudflare.com>
2023-02-23 12:55:59 +0000

    🧹 Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
 
 interface OutbrainParams {
-  apiVersion: string,
-  obtpVersion: string,
-  optOut: boolean,
-  bust: string,
-  dl: string,
-  ob_click_id?: string | null,
+  apiVersion: string
+  obtpVersion: string
+  optOut: boolean
+  bust: string
+  dl: string
+  ob_click_id?: string | null
 }
 
-export const eventHandler = async (eventType: string, event: MCEvent, settings: ComponentSettings) => {
+export const eventHandler = async (
+  eventType: string,
+  event: MCEvent,
+  settings: ComponentSettings
+) => {
   const { client, payload } = event
 
   const baseUrl = 'https://tr.outbrain.com/'
   const unifiedPixel = 'unifiedPixel'
 
-  let params: OutbrainParams = {
+  const params: OutbrainParams = {
     apiVersion: '1.1',
     obtpVersion: '1.4.1', // what is this?
     optOut: false,
@@ -30,7 +34,10 @@ export const eventHandler = async (eventType: string, event: MCEvent, settings: 
   }
 
   payload.name = payload.name.replace(/ /g, '-')
-  const stringifiedParams = new URLSearchParams({ ...payload, ...params }).toString()
+  const stringifiedParams = new URLSearchParams({
+    ...payload,
+    ...params,
+  }).toString()
 
   fetch(`${baseUrl}${unifiedPixel}?${stringifiedParams}`)
 }
